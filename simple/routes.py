4e1c582dddb952096ee5c21ed9670d8856215bd2
jod35 <jodestrevin@gmail.com>
2020-07-16 06:24:09 -0700

    added the basic routes

--- simple/routes.py
+++ simple/routes.py
@@ -2,5 +2,18 @@ from simple import app,db
 from flask import render_template,request,url_for,redirect
 
 @app.route('/')
-def hello():
-    return "Hello"
+def index():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('signup.html')
+
+@app.route('/profile')
+def user_profile():
+    return render_template('profile.html')
+
